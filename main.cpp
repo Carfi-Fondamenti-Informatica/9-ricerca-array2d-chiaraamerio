7454026a72842e8ae161ee29566efa9c495a9e56
chiaraamerio <100305899+chiaraamerio@users.noreply.github.com>
2022-05-09 19:03:09 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
+#include "lib.h";
 
 int main(){
+   char lista_nomi [10][20];
+    char nome [20];
+
+    init (lista_nomi, 10);
+    init (nome, 20);
+
+    for (int i=0; i<10; i++){
+        cin>>lista_nomi[i];
+    }
+    cin>> nome;
+
+    int z= ricerca (lista_nomi, nome);
+    if (z==-1){
+        cout<<"non presente"<<endl;
+    } else {
+        cout<<z<<endl;
+    }
+  
   return 0;
 }
